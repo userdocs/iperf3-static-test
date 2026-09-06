2d0413967d44fd39c1a0924461e70cc90175e1e7
userdocs <16525024+userdocs@users.noreply.github.com>
2026-09-06 09:56:29 +0100

    Update ci-windows-cygwin-build.sh

--- ci-windows-cygwin-build.sh
+++ ci-windows-cygwin-build.sh
@@ -103,8 +103,6 @@ if [[ ${only_apps} == "only_iperf3" ]]; then
 		./configure --disable-shared --enable-static --enable-static-bin --prefix="$HOME/iperf3"
 	fi
 
-	cat config.log
-
 	printf '\n%b\n\n' " \e[94m\U25cf\e[0m make"
 	make -j"$(nproc)"
 
